fa2960d53286b9ec97114426793f8d2b00e2faab
Moke Zhang <17319370515@163.com>
2024-08-09 10:56:58 +0800

    Refactor imshow.cc for improved image display and color conversion

--- minimal/imshow.cc
+++ minimal/imshow.cc
@@ -1,7 +1,12 @@
 #include "InputFiles.hpp"
-
+#include "opencv2/opencv.hpp"
 
 int main() {
     const uint8_t* img = GetImgArray(0);
+    cv::Mat src(128, 128, CV_8UC3, (void*)img);
+    cv::Mat dst;
+    cv::cvtColor(src, dst, cv::COLOR_RGB2BGR);
+    cv::imshow("Image", dst);
+    cv::waitKey(0);
     return 0;
 }

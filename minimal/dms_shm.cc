c5dbb794a67704c23c8477435721ae414d93466d
Moke Zhang <17319370515@163.com>
2024-08-26 16:25:37 +0800

    refactor: Resize and crop image for improved performance and readability

--- minimal/dms_shm.cc
+++ minimal/dms_shm.cc
@@ -2,6 +2,37 @@
 #include <opencv2/opencv.hpp>
 
 
+std::vector<uint8_t> 
+// cv::Mat
+resizeCropImage(const cv::Mat& originalImage, const cv::Size& imageSize) {
+    // IFM size
+    int ifmWidth = imageSize.width;
+    int ifmHeight = imageSize.height;
+
+    // Aspect ratio resize
+    float scaleRatio = static_cast<float>(std::max(ifmWidth, ifmHeight)) / 
+                       static_cast<float>(std::min(originalImage.cols, originalImage.rows));
+    int resizedWidth = static_cast<int>(originalImage.cols * scaleRatio);
+    int resizedHeight = static_cast<int>(originalImage.rows * scaleRatio);
+    cv::Mat resizedImage;
+    cv::resize(originalImage, resizedImage, cv::Size(resizedWidth, resizedHeight), 0, 0, cv::INTER_LINEAR);
+
+    // Crop the center of the image
+    int left = (resizedWidth - ifmWidth) / 2;
+    int top = (resizedHeight - ifmHeight) / 2;
+    cv::Rect cropRegion(left, top, ifmWidth, ifmHeight);
+    cv::Mat croppedImage = resizedImage(cropRegion);
+
+    // return croppedImage;
+    // Convert to a flattened array
+    std::vector<uint8_t> flattenedImage;
+    // flattenedImage.assign(croppedImage.datastart, croppedImage.dataend);
+    flattenedImage.assign(croppedImage.data, croppedImage.data + croppedImage.total() * croppedImage.elemSize());
+    
+    return flattenedImage;
+}
+
+
 int main() {
 
     std::string rtsp1 = "rtmp://172.20.10.3:10035/live/sVNsJvqSR";
@@ -24,7 +55,12 @@ int main() {
             continue;
         }
 
-        cv::imshow("input", frame1);
+        cv::Size imageSize(128, 128);
+        
+        std::vector<uint8_t> data = resizeCropImage(frame1, imageSize);
+        cv::Mat croppedImage = cv::Mat(imageSize, CV_8UC3, data.data());
+
+        cv::imshow("cropped image", croppedImage);
 
         if (cv::waitKey(1) == 'q')
         {
